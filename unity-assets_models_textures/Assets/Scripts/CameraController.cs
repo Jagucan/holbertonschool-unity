dee159363464dc853642a34cd70631bb269914d6
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-19 18:41:39 -0500

    control camera

---
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float camSpeedRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cursor Options.
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controls for Player Camera Rotation.
+        float mouseX = Input.GetAxis("Mouse X") * camSpeedRotation * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 {
-    private CharacterController controller;
+    public CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     public float playerSpeed;
     public float jumpHeight;
     private readonly float gravityValue = -9.81f;
-
-    private void Start()
-    {
-        controller = gameObject.AddComponent<CharacterController>();
-    }
 
     void Update()
     {
@@ -25,13 +20,11 @@ public class Example : MonoBehaviour
         }
 
         // Changes the Horizontal and Vertical Position of the Player.
-        Vector3 move = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(playerSpeed * Time.deltaTime * move);
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        controller.Move(moveZ * playerSpeed * Time.deltaTime * transform.forward);
+        controller.Move(moveX * playerSpeed * Time.deltaTime * transform.right);
 
         // Changes the height position of the player (Jump).
         if (Input.GetButtonDown("Jump") && groundedPlayer)
