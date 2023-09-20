79b273241642cec84d75b9085440d93ac3f1d2cf
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-09-20 15:02:36 -0500

    create canvas timer

---
+++ unity-assets_models_textures/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject newPlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            player.SetActive(false);
+            newPlayer.SetActive(true);
+            StartCoroutine(LoadScene(2));
+        }
+    }
+    void RestartScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        RestartScene();
+    }
+}
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Example : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Example : MonoBehaviour
     public float playerSpeed;
     public float jumpHeight;
     private readonly float gravityValue = -9.81f;
+
 
     void Update()
     {
